dadd7c2f496b340d1daa4620535cfe29efb49cd8
Rawlinrepo <rawlinleitao08@gmail.com>
2025-08-12 12:02:22 +0530

    Create push.groovy

---
+++ vars/push.groovy
@@ -0,0 +1,12 @@
+def call(){
+  echo "pushing Stage"
+  withCredentials([usernamePassword(
+  credentialsId : 'dockerHubCred',
+  usernameVariable: "username", 
+  passwordVariable: "password"
+  )]){
+  sh '''
+  docker login -u $username -p $password
+  docker push $username/frontend:latest
+  '''
+}
